a2ce8f8cbd0593a6504d4d5d0dd2defae922bb6c
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2021-11-22 01:39:45 +0900

    프로그래머스 스택/큐 - 기능개발
    for문을 3번이나 사용했다.
    시간초과는 안 걸렸지만 좀 더 공부해야겠다.
    프로그래머스에 있는 다른 분 코드 보고 배운 점:
    1. if문 사용 없이 나머지가 0인 것과 1인 것을 계산해냄.
    day = (99 - progresses[i]) / speeds[i] + 1;
    2. for문을 한 번만 사용함.

---
+++ 기능개발.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+
+    vector<int> day;
+    for (int i = 0; i < progresses.size(); i++) {
+        day.push_back(100 - progresses[i]);
+    }
+    for (int i = 0; i < day.size(); i++) {
+        if (day[i] % speeds[i] == 0) {
+            day[i] = day[i] / speeds[i];
+        }
+        else {
+            day[i] = day[i] / speeds[i] + 1; //나머지 있으니까 하루 더해줌.
+        }
+    }
+    int val = day[0];
+    int cnt = 1;
+    for (int i = 1; i < day.size(); i++) {
+        if (val >= day[i]) {
+            cnt++;
+        }
+        else {
+            answer.push_back(cnt);
+            val = val + (day[i] - val);
+            cnt = 1;
+        }
+    }
+    
+    answer.push_back(cnt);
+
+
+    return answer;
+}

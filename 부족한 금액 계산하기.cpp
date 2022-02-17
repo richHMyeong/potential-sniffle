0b3d673672646ebf7ccfc834248e41fe334eb291
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2022-02-17 14:48:08 +0900

    프로그래머스 연습문제 부족한 금액 계산하기
    간단한 문제였으나 함수 반환 형태가 long long인 것을 간과함.
    한번 틀린 후 answer, sum 모두 int에서 long long으로 변경함.

---
+++ 부족한 금액 계산하기.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+
+    long long sum = 0;
+    for (int i = 1; i <= count; i++) {
+        sum = sum + (price * i);
+    }
+
+    if (money - sum >= 0) {
+        answer = 0;
+    }
+    else {
+        answer = (money - sum) * -1;
+
+    }
+
+    return answer;
+}

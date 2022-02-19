1f4ddeadde809ce2c0e1e494076683bc8fa9338f
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2022-02-19 18:27:38 +0900

    프로그래머스 연습문제 약수의 합
    주어진 수/2까지만 for반복문을 돌리면 된다.

---
+++ 약수의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(int n) {
+	int answer = 0;
+
+	for (int i = 1; i <= n / 2; i++) {
+		if (n % i == 0) {
+			answer = answer + i;
+		}
+	}
+	answer = answer + n;
+
+	return answer;
+}

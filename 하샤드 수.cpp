007dd80955915281eb3d196a8fe36dd5eb715acc
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2022-02-24 22:02:39 +0900

    프로그래머스 연습문제 하샤드 수
    하샤드 수 : 양의 정수 x의 자릿수의 합으로 x가 나누어 떨어짐.

---
+++ 하샤드 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    
+    int n=x;
+    int sum=0;
+    while(n!=0){
+        sum=sum+n%10;
+        n=n/10;
+    }
+    if(x%sum!=0){
+        answer=false;
+    }
+    
+    return answer;
+}

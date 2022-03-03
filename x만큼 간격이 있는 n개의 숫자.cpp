1995119ebda60f64ac896be0aaea4752c08429a7
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2022-03-03 20:42:54 +0900

    프로그래머스 연습문제 x만큼 간격이 있는 n개의 숫자
    반복문을 사용하여 풀었다.

---
+++ x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int tmp=x;
+    for (int i=0;i<n;i++){
+        answer.push_back(tmp);
+        tmp=tmp+x;
+    }
+    
+    
+    return answer;
+}

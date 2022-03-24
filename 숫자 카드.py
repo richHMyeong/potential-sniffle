2bab114780c5517aeaff231d2cca5db551ed861f
richHMyeong <86832485+richHMyeong@users.noreply.github.com>
2022-03-24 21:21:40 +0900

    백준 정렬 숫자 카드
    정렬 카테고리로 들어갔지만 오히려 이분 탐색 연습이 되었던 문제.
    처음에는 이코테 책 코드 참고해서 풀었는데 자꾸 런타임에러 떠서 구글링 해서 다른 분이 푸신 코드 보고 여기저기 고쳤다.
    이분 탐색 좀 더 공부해야할 것 같다.

---
+++ 숫자 카드.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(input())
+cards=list(map(int, sys.stdin.readline().split()))
+m=int(input())
+checks=list(map(int, sys.stdin.readline().split()))
+
+cards.sort()
+
+def binary_search(array, target, start, end):
+    while start<=end:
+        mid=(start+end)//2
+
+        if array[mid]==target:
+            return mid
+        elif array[mid] > target:
+            end = mid-1
+        else :
+            start=mid+1
+    return None
+
+for i in range(m):
+    if binary_search(cards, checks[i], 0, n-1) is not None:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+
+
